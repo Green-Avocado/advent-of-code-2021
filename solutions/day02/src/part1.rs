413f60cb4e905a04433d4519021f84d85d5164c6
Green-Avocado <greenavocado@protonmail.com>
2021-12-01 21:54:21 -0800

    added day 2 solution

---
+++ solutions/day02/src/part1.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
+    let mut horizontal_pos = 0;
+    let mut vertical_pos: i32 = 0;
+
+    for line in lines {
+        if let Ok(s) = line {
+            let mut split = s.split(" ");
+            let direction = split.next().unwrap();
+            let distance: u32 = split.next().unwrap().parse().unwrap();
+
+            match direction {
+                "forward" => horizontal_pos += distance,
+                "up" => vertical_pos -= distance as i32,
+                "down" => vertical_pos += distance as i32,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    horizontal_pos as i64 * vertical_pos as i64
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(150, solution(crate::get_input("test")));
+    }
+}
---
+++ solutions/day02/src/part2.rs
@@ -0,0 +1,40 @@
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
+    let mut horizontal_pos = 0;
+    let mut vertical_pos: i32 = 0;
+    let mut aim = 0;
+
+    for line in lines {
+        if let Ok(s) = line {
+            let mut split = s.split(" ");
+            let direction = split.next().unwrap();
+            let distance: u32 = split.next().unwrap().parse().unwrap();
+
+            match direction {
+                "forward" => {
+                    horizontal_pos += distance;
+                    vertical_pos += aim * distance as i32;
+                }
+                "up" => aim -= distance as i32,
+                "down" => aim += distance as i32,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    horizontal_pos as i64 * vertical_pos as i64
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(900, solution(crate::get_input("test")));
+    }
+}
