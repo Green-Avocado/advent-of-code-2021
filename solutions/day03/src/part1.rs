460d2cd31981aa45d3ef4206f5f300203ec94fe9
Green-Avocado <greenavocado@protonmail.com>
2021-12-02 22:15:46 -0800

    solved part 1

--- solutions/day03/src/part1.rs
+++ solutions/day03/src/part1.rs
@@ -3,15 +3,47 @@ use std::{
     io::{BufReader, Lines},
 };
 
-pub fn solution(mut lines: Lines<BufReader<File>>) -> i64 {
-    let mut ans = 0;
+pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
+    let mut length = 0;
+    let mut totals = Vec::new();
 
     for line in lines {
         if let Ok(s) = line {
+            let mut i = 0;
+
+            for c in s.chars() {
+                let n = if c == '1' {
+                    1
+                } else {
+                    0
+                };
+
+                match totals.get_mut(i) {
+                    Some(x) => *x += n,
+                    None => totals.push(n),
+                }
+                i += 1;
+            }
         }
+
+        length += 1;
     }
 
-    ans
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    let mut multiplier = 1;
+
+    for n in totals.iter().rev() {
+        if *n * 2 > length {
+            gamma += multiplier;
+        } else {
+            epsilon += multiplier;
+        }
+        
+        multiplier *= 2;
+    }
+
+    gamma * epsilon
 }
 
 #[cfg(test)]
