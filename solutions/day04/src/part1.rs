85e0b81aa8df65f8508d1782e797ec5534ec17e0
Green-Avocado <greenavocado@protonmail.com>
2021-12-03 23:29:36 -0800

    added day 4 starter files

---
+++ solutions/day04/src/part1.rs
@@ -0,0 +1,23 @@
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
+    for line in lines {
+        if let Ok(s) = line {
+        }
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(0, solution(crate::get_input("test")));
+    }
+}
