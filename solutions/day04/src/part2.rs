7481da72aa2ffe212db966cdeb57320b4eb05c04
Green-Avocado <greenavocado@protonmail.com>
2021-12-04 15:55:49 -0800

    solved part 2

--- solutions/day04/src/part2.rs
+++ solutions/day04/src/part2.rs
@@ -1,15 +1,36 @@
+use crate::utils;
+
 use std::{
     fs::File,
     io::{BufReader, Lines},
 };
 
 pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
-    for line in lines {
-        if let Ok(s) = line {
+    let (nums, mut boards) = utils::parse_input(lines);
+    let mut last_score = 0;
+
+    for num in &nums {
+        let mut i = 0;
+        let mut to_remove = Vec::new();
+
+        for board in &mut boards {
+            if let Some(x) = board.check_num(*num) {
+                last_score = x;
+                to_remove.push(i);
+            }
+            i += 1;
+        }
+
+        for index in to_remove.iter().rev() {
+            boards.remove(*index);
+        }
+
+        if boards.is_empty() {
+            break;
         }
     }
 
-    0
+    last_score
 }
 
 #[cfg(test)]
@@ -18,6 +39,6 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(crate::get_input("test")));
+        assert_eq!(1924, solution(crate::get_input("test")));
     }
 }
