067515465d19819cc5f09b8d7244ddd287889274
Green-Avocado <greenavocado@protonmail.com>
2021-12-05 01:27:01 -0800

    added starter files

---
+++ solutions/day05/src/part1.rs
@@ -0,0 +1,20 @@
+use crate::utils;
+
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(0, solution(crate::get_input("test")));
+    }
+}
---
+++ solutions/day05/src/utils.rs
@@ -0,0 +1,11 @@
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+pub fn parse_input(mut lines: Lines<BufReader<File>>) {
+    for line in lines {
+        if let Ok(s) = line {
+        }
+    }
+}
