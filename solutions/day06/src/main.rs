173c3e059933d54d6f2a7b64644b09ad33f6d346
Green-Avocado <greenavocado@protonmail.com>
2021-12-05 23:12:02 -0800

    added starter files

---
+++ solutions/day06/src/main.rs
@@ -0,0 +1,28 @@
+mod part1;
+mod part2;
+
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn get_input(filename: &str) -> [u16; 9] {
+    let input = File::open(format!("./data/{}", filename)).expect("Could not read file");
+
+    let nums_string = BufReader::new(input).lines().next().unwrap().unwrap();
+    let mut nums = [0; 9];
+
+    for num_str in nums_string.split(',') {
+        let i: usize = num_str.parse().unwrap();
+        nums[i] += 1;
+    }
+
+    nums
+}
+
+fn main() {
+    let nums = get_input("input");
+
+    println!("Part 1: {}", part1::solution(nums.clone()));
+    println!("Part 2: {}", part2::solution(nums.clone()));
+}
---
+++ solutions/day06/src/part1.rs
@@ -0,0 +1,16 @@
+pub fn solution(nums: [u16; 9]) -> i64 {
+    for num in nums {
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(5934, solution(crate::get_input("test")));
+    }
+}
