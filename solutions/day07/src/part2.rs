04aa024a976f2c8a5f65d370c90a27dd2a1172b7
Green-Avocado <greenavocado@protonmail.com>
2021-12-07 11:54:58 -0800

    solved part 2

--- solutions/day07/src/part2.rs
+++ solutions/day07/src/part2.rs
@@ -1,5 +1,37 @@
 pub fn solution(frequencies: &Vec<u32>) -> i64 {
-    0
+    let mut iterator = frequencies.iter();
+
+    let mut left = *iterator.next().unwrap();
+    let mut right = *iterator.next_back().unwrap();
+
+    let mut left_freq = left;
+    let mut right_freq = right;
+
+    let mut total = 0;
+
+    loop {
+        if left > right {
+            total += right;
+
+            if let Some(x) = iterator.next_back() {
+                right_freq += *x;
+                right += right_freq;
+            } else {
+                break;
+            }
+        } else {
+            total += left;
+
+            if let Some(x) = iterator.next() {
+                left_freq += *x;
+                left += left_freq;
+            } else {
+                break;
+            }
+        }
+    }
+
+    total as i64
 }
 
 #[cfg(test)]
@@ -8,6 +40,6 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(&crate::get_input("test")));
+        assert_eq!(168, solution(&crate::get_input("test")));
     }
 }
