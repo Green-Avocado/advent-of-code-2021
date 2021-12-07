d5407d964aaabd2f577bdd4f4be4ba5fdd5921b4
Green-Avocado <greenavocado@protonmail.com>
2021-12-07 11:18:29 -0800

    solved part 1

--- solutions/day07/src/part1.rs
+++ solutions/day07/src/part1.rs
@@ -1,5 +1,42 @@
 pub fn solution(nums: &Vec<u16>) -> i64 {
-    0
+    let mut frequencies = Vec::new();
+
+    for n in nums {
+        while *n as usize >= frequencies.len() {
+            frequencies.push(0);
+        }
+
+        *frequencies.get_mut(*n as usize).unwrap() += 1;
+    }
+
+    let mut iterator = frequencies.iter();
+
+    let mut left = *iterator.next().unwrap();
+    let mut right = *iterator.next_back().unwrap();
+
+    let mut total = 0;
+
+    loop {
+        if left > right {
+            total += right;
+
+            if let Some(x) = iterator.next_back() {
+                right += *x;
+            } else {
+                break;
+            }
+        } else {
+            total += left;
+
+            if let Some(x) = iterator.next() {
+                left += *x;
+            } else {
+                break;
+            }
+        }
+    }
+
+    total as i64
 }
 
 #[cfg(test)]
