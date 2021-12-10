51907fecb8b61a6a1ba627720b9c4fa37275a900
Green-Avocado <greenavocado@protonmail.com>
2021-12-10 03:36:25 -0800

    fixed imports

--- solutions/day09/src/part1.rs
+++ solutions/day09/src/part1.rs
@@ -3,17 +3,17 @@ use std::{
     io::{BufReader, Lines},
 };
 
-use crate::utils::{self, get_local_mins};
+use crate::utils::{get_local_mins, line_result_to_vec};
 
 pub fn solution(mut lines: Lines<BufReader<File>>) -> i64 {
     let mut ans = 0;
 
     let mut prev_nums = None;
-    let mut next_nums = utils::line_result_to_vec(lines.next());
+    let mut next_nums = line_result_to_vec(lines.next());
 
     loop {
         if let Some(x) = next_nums {
-            next_nums = utils::line_result_to_vec(lines.next());
+            next_nums = line_result_to_vec(lines.next());
 
             get_local_mins(&x, prev_nums.as_ref(), next_nums.as_ref())
                 .iter()
@@ -30,6 +30,8 @@ pub fn solution(mut lines: Lines<BufReader<File>>) -> i64 {
 
 #[cfg(test)]
 mod tests {
+    use crate::utils;
+
     use super::*;
 
     #[test]
