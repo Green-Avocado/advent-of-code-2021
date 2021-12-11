688c7cc85076ff39628ee7e01dfc9832e37f2006
Green-Avocado <greenavocado@protonmail.com>
2021-12-11 09:23:31 -0800

    solved part 2

--- solutions/day10/src/part2.rs
+++ solutions/day10/src/part2.rs
@@ -4,12 +4,50 @@ use std::{
 };
 
 pub fn solution(lines: Lines<BufReader<File>>) -> i64 {
-    for line in lines {
+    let mut scores = Vec::new();
+
+    'lines_loop: for line in lines {
         if let Ok(s) = line {
+            let mut stack = Vec::new();
+
+            for c in s.chars() {
+                match c {
+                    '(' | '[' | '{' | '<' => stack.push(match c {
+                        '(' => ')',
+                        '[' => ']',
+                        '{' => '}',
+                        '<' => '>',
+                        _ => unreachable!(),
+                    }),
+                    ')' | ']' | '}' | '>' => {
+                        if c != stack.pop().unwrap() {
+                            continue 'lines_loop;
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            let mut score = 0;
+
+            for c in stack.iter().rev() {
+                score *= 5;
+                score += match *c {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => unreachable!(),
+                }
+            }
+
+            scores.push(score);
         }
     }
 
-    0
+    scores.sort();
+
+    *scores.get(scores.len() / 2).unwrap()
 }
 
 #[cfg(test)]
@@ -18,6 +56,6 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(crate::utils::get_input("test")));
+        assert_eq!(288957, solution(crate::utils::get_input("test")));
     }
 }
