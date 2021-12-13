5fbad2159c1655575d223f6d56fec7b684bdc07e
Green-Avocado <greenavocado@protonmail.com>
2021-12-12 23:05:00 -0800

    solved part 1

--- solutions/day11/src/part1.rs
+++ solutions/day11/src/part1.rs
@@ -1,5 +1,18 @@
+use crate::utils;
+
+const STEPS: u8 = 100;
+
 pub fn solution(grid: &Vec<Vec<u8>>) -> i64 {
-    0
+    let mut octopi = grid.clone();
+    let mut flashes = 0;
+
+    for _i in 0..STEPS {
+        utils::grid_increment_all(&mut octopi);
+        flashes += utils::grid_flash_all(&mut octopi) as i64;
+        utils::grid_reset_flashes(&mut octopi);
+    }
+
+    flashes
 }
 
 #[cfg(test)]
@@ -8,6 +21,6 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(&crate::utils::get_input("test")));
+        assert_eq!(1656, solution(&crate::utils::get_input("test")));
     }
 }
--- solutions/day11/src/utils.rs
+++ solutions/day11/src/utils.rs
@@ -22,3 +22,105 @@ pub fn get_input(filename: &str) -> Vec<Vec<u8>> {
 
     grid
 }
+
+pub fn grid_increment_all(grid: &mut Vec<Vec<u8>>) {
+    for row in grid {
+        for col in row {
+            *col += 1;
+        }
+    }
+}
+
+pub fn grid_flash_all(grid: &mut Vec<Vec<u8>>) -> u8{
+    let mut flashes = 0;
+
+    let mut row_num = 0;
+
+    'row_loop: loop {
+        let mut col_num = 0;
+
+        'col_loop: loop {
+            if let Some(x) = grid.get(row_num) {
+                if let Some(y) = x.get(col_num) {
+                    if *y == 10 {
+                        flashes += grid_flash(grid, row_num, col_num);
+                    }
+
+                    col_num += 1;
+                } else {
+                    break 'col_loop;
+                }
+            } else {
+                break 'row_loop;
+            }
+        }
+
+        row_num += 1;
+    }
+
+    flashes
+}
+
+fn grid_flash(grid: &mut Vec<Vec<u8>>, row: usize, col: usize) -> u8 {
+    let mut flashes = 1;
+
+    let mut row_num = if row > 0 {
+        row - 1
+    } else {
+        0
+    };
+
+    'row_loop: loop {
+        if row_num > row + 1 {
+            break;
+        }
+
+        let mut col_num = if col > 0 {
+            col - 1
+        } else {
+            0
+        };
+
+        'col_loop: loop {
+            if col_num > col + 1 {
+                break;
+            }
+
+            if let Some(x) = grid.get_mut(row_num) {
+                if let Some(y) = x.get_mut(col_num) {
+                    if row_num == row && col_num == col {
+                        *y = 11;
+                    }
+
+                    if *y < 10 {
+                        *y += 1;
+
+                        if *y == 10 {
+                            flashes += grid_flash(grid, row_num, col_num);
+                        }
+                    }
+
+                    col_num += 1;
+                } else {
+                    break 'col_loop;
+                }
+            } else {
+                break 'row_loop;
+            }
+        }
+
+        row_num += 1;
+    }
+
+    flashes
+}
+
+pub fn grid_reset_flashes(grid: &mut Vec<Vec<u8>>) {
+    for x in grid {
+        for y in x {
+            if *y > 9 {
+                *y = 0;
+            }
+        }
+    }
+}
