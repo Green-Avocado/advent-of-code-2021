047cf32cc6ef3d4f8bce35ee367e5d03ea61e37b
Green-Avocado <greenavocado@protonmail.com>
2021-12-14 13:27:11 -0800

    solved part 2

--- solutions/day12/src/part2.rs
+++ solutions/day12/src/part2.rs
@@ -1,7 +1,31 @@
 use std:: collections::HashMap;
 
 pub fn solution(graph: &HashMap<String, Vec<String>>) -> i64 {
-    0
+    count_paths(graph, &"start".to_string(), Vec::new(), true)
+}
+
+fn count_paths(graph: &HashMap<String, Vec<String>>, node: &String, mut path: Vec<String>, extra_visit: bool) -> i64 {
+    if node == "end" {
+        return 1;
+    }
+
+    let mut count = 0;
+    path.push(node.clone());
+
+    let branches = graph.get(node).unwrap();
+
+    for branch in branches {
+        if branch == "start" {
+            continue;
+        }
+
+        let condition = branch.to_uppercase() == *branch || !path.contains(branch);
+        if condition || extra_visit {
+            count += count_paths(graph, branch, path.clone(), condition && extra_visit);
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -9,9 +33,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
-        assert_eq!(0, solution(&crate::utils::get_input("test0")));
-        assert_eq!(0, solution(&crate::utils::get_input("test1")));
-        assert_eq!(0, solution(&crate::utils::get_input("test2")));
+    fn sample0() {
+        assert_eq!(36, solution(&crate::utils::get_input("test0")));
+    }
+
+    #[test]
+    fn sample1() {
+        assert_eq!(103, solution(&crate::utils::get_input("test1")));
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(3509, solution(&crate::utils::get_input("test2")));
     }
 }
