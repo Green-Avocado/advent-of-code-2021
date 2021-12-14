1f7559c986c038daba9a71a1358dcce170ad5c37
Green-Avocado <greenavocado@protonmail.com>
2021-12-14 13:03:30 -0800

    solved part 1

--- solutions/day12/src/part1.rs
+++ solutions/day12/src/part1.rs
@@ -1,7 +1,26 @@
 use std:: collections::HashMap;
 
 pub fn solution(graph: &HashMap<String, Vec<String>>) -> i64 {
-    0
+    count_paths(graph, &"start".to_string(), Vec::new())
+}
+
+fn count_paths(graph: &HashMap<String, Vec<String>>, node: &String, mut path: Vec<String>) -> i64 {
+    if node == "end" {
+        return 1;
+    }
+
+    let mut count = 0;
+    path.push(node.clone());
+
+    let branches = graph.get(node).unwrap();
+
+    for branch in branches {
+        if !path.contains(branch) || branch.to_uppercase() == *branch {
+            count += count_paths(graph, branch, path.clone());
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -9,9 +28,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
+    fn sample0() {
         assert_eq!(10, solution(&crate::utils::get_input("test0")));
+    }
+
+    #[test]
+    fn sample1() {
         assert_eq!(19, solution(&crate::utils::get_input("test1")));
+    }
+
+    #[test]
+    fn sample2() {
         assert_eq!(226, solution(&crate::utils::get_input("test2")));
     }
 }
