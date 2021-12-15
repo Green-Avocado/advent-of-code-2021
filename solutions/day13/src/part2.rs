0bd13185d0530a00db63970cf96bef5887396a86
Green-Avocado <greenavocado@protonmail.com>
2021-12-14 20:18:05 -0800

    solved part 2

--- solutions/day13/src/part2.rs
+++ solutions/day13/src/part2.rs
@@ -1,7 +1,40 @@
 use std::collections::HashSet;
 
-pub fn solution((points, folds): &(HashSet<(u16, u16)>, Vec<(u16, bool)>)) -> i64 {
-    0
+pub fn solution((points, folds): &(HashSet<(u16, u16)>, Vec<(u16, bool)>)) -> String {
+    let mut current_points = points.clone();
+
+    for fold in folds {
+        current_points = crate::utils::fold_paper(&current_points, fold);
+    }
+
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    for &(x, y) in &current_points {
+        if x > max_x {
+            max_x = x;
+        }
+
+        if y > max_y {
+            max_y = y;
+        }
+    }
+
+    let mut s = "\n".to_string();
+
+    for y in 0..=max_y {
+        for x in 0..=max_x {
+            if current_points.contains(&(x, y)) {
+                s.push('#');
+            } else {
+                s.push('.');
+            }
+        }
+
+        s.push('\n');
+    }
+
+    s
 }
 
 #[cfg(test)]
@@ -10,6 +43,12 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(&crate::utils::get_input("test")));
+        assert_eq!("\n\
+        #####\n\
+        #...#\n\
+        #...#\n\
+        #...#\n\
+        #####\n\
+        ", solution(&crate::utils::get_input("test")));
     }
 }
