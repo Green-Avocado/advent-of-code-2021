3b34f5597f22640822e675e70bd2dddd10f1953e
Green-Avocado <greenavocado@protonmail.com>
2021-12-14 17:10:14 -0800

    added input parsing

--- solutions/day13/src/utils.rs
+++ solutions/day13/src/utils.rs
@@ -5,11 +5,28 @@ use std::{
 
 pub fn get_input(filename: &str) -> (Vec<(u16, u16)>, Vec<(u16, bool)>) {
     let input = File::open(format!("./data/{}", filename)).expect("Could not read file");
+    let mut lines = BufReader::new(input).lines();
     let mut points = Vec::new();
     let mut folds = Vec::new();
     
-    for line in BufReader::new(input).lines() {
-        if let Ok(s) = line {
+    while let Some(Ok(s)) = lines.next() {
+        if let Some((a, b)) = s.split_once(',') {
+            points.push((a.parse().unwrap(), b.parse().unwrap()));
+        } else {
+            break;
+        }
+    }
+
+    while let Some(Ok(s)) = lines.next() {
+        if let Some(fold) = s.strip_prefix("fold along ") {
+            let (a, b) = fold.split_once('=').unwrap();
+
+            let horizontal = a == "y";
+            let location = b.parse().unwrap();
+
+            folds.push((location, horizontal));
+        } else {
+            break;
         }
     }
 
