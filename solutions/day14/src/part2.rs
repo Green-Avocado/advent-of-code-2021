d0a787bc1883ae735c84bf3cdda65de8b09075ab
Green-Avocado <greenavocado@protonmail.com>
2021-12-16 21:44:38 -0800

    solved part 2

--- solutions/day14/src/part2.rs
+++ solutions/day14/src/part2.rs
@@ -1,7 +1,15 @@
 use std::collections::HashMap;
 
+use crate::utils::{apply_substitutions, get_freq_diff, get_pairs};
+
 pub fn solution((template, rules): &(String, HashMap<(char, char), char>)) -> i64 {
-    0
+    let mut pair_map = get_pairs(template);
+
+    for _i in 0..40 {
+        pair_map = apply_substitutions(pair_map, rules);
+    }
+
+    get_freq_diff(template, pair_map) as i64
 }
 
 #[cfg(test)]
@@ -10,6 +18,6 @@ mod tests {
 
     #[test]
     fn sample() {
-        assert_eq!(0, solution(&crate::utils::get_input("test")));
+        assert_eq!(2188189693529, solution(&crate::utils::get_input("test")));
     }
 }
